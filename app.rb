e9a7baaeb79bb4ab1e416f02ebb166d6b6da2ddc
Wineshuga <uzochukwuwinnie@gmail.com>
2023-10-18 10:17:31 +0100

    Add list_music_album and add_music_album methods

--- app.rb
+++ app.rb
@@ -9,11 +9,39 @@ class App
 
   def list_genre
     if @genre.empty?
-      puts "No genre found. Add a genre"
+      puts "No genres found. Add a genre"
     else
       @genre.each_with_index do |genre, index|
         puts "#{index + 1}) Genre: \"#{genre}\""
       end
     end
   end
+
+  def list_music_album
+    if @music_album.empty?
+      puts "No music albums found. Add a music album"
+    else
+      @music_album.each_with_index do |album, index|
+        puts "#{index + 1}) Music Album: \"#{album}\""
+      end
+    end
+  end
+
+  def add_music_album
+    print 'Is it on Spotify(Y/N): '
+    spotify = gets.chomp
+    if spotify.lowercase == 'y'
+      spotify = true
+    elsif spotify.lowercase == 'n'
+      spotify = false
+    else
+      print 'Invalid answer'
+      exit
+    end
+    puts "What's the genre?: "
+    genre_name = gets.chomp
+    new_genre = MusicAlbum.new(spotify, genre_name)
+    print 'Music album created!'
+  end
+
 end
