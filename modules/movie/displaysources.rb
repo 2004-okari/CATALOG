b9d511848e572a43f224b1fe9a0f007de16feab4
Newton Ombese <newtonombese1@gmail.com>
2023-10-18 12:23:59 +0300

    Add movies modules

---
+++ modules/movie/displaysources.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+module DisplaySources
+  def display_all_sources
+    @sources.each_with_index do |source, index|
+      puts "#{index}) Source Name: #{source.name}"
+    end
+  end
+end
