897952e40bf662707da487349b668ac0c5ddfaf2
okari <nyandika15@gmail.com>
2023-10-18 13:19:18 +0300

    Update app.rb

--- app.rb
+++ app.rb
@@ -1,5 +1,11 @@
 require_relative '../classes/genre'
 require_relative '../classes/music_album'
+require_relative '../classes/game'
+require_relative '../classes/author'
+require_relative '../classes/book'
+require_relative '../classes/label'
+require_relative '../classes/movie'
+require_relative '../classes/source'
 
 class App
   def initialize
