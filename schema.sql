0ca96b5ab03226f4e635abc28b24dd356b9406d5
Newton Ombese <newtonombese1@gmail.com>
2023-10-18 11:34:26 +0300

    Add source and movies schema

--- schema.sql
+++ schema.sql
@@ -18,3 +18,20 @@ CREATE TABLE Games (
 
     FOREIGN KEY (author_id) REFERENCES Authors(id)
 );
+
+CREATE TABLE source
+(
+    id INT PRIMARY KEY NOT NULL,
+    name VARCHAR(100),
+    items TEXT
+    []
+);
+
+CREATE TABLE movie
+(
+    id INT PRIMARY KEY NOT NULL,
+    silent BOOLEAN,
+    publish_date DATE,
+    archived BOOLEAN,
+    source_id INT REFERENCES source(id)
+);
