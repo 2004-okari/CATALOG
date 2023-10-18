9a6c271cbde6d9c44d4c64fdb53067407d00c8de
Newton Ombese <newtonombese1@gmail.com>
2023-10-18 19:17:30 +0300

    Working for loading of sources and movies

--- classes/loading.rb
+++ classes/loading.rb
@@ -18,4 +18,41 @@ class Saving
       music_album << MusicAlbum.new(album['spotify'], album['genre'])
     end
   end
+
+  def load_books(books)
+    return unless File.exist?('data/books.json')
+
+    retrieved_books = JSON.parse(File.read('data/books.json'))
+    retrieved_books.each do |book_data|
+      book = Book.new(book_data['publish_date'], book_data['publisher'], book_data['cover_state'])
+      books << book
+    end
+  end
+
+  def load_sources
+    source_hash = []
+    return source_hash unless File.exist?('data/source.json')
+
+    source_hash = JSON.parse(File.read('data/source.json'))
+
+    source_hash.each do |source|
+      source_obj = Source.new(source['name'])
+      source_obj.id = source['id']
+      @sources << source_obj
+    end
+  end
+
+  def load_movies
+    movie_hash = []
+    return movie_hash unless File.exist?('data/movie.json')
+
+    movie_hash = load_data_from_file('data/movie.json')
+    movie_hash.each do |movie|
+      movie_obj = Movie.new(movie['publish_date'], silent: movie['silent'])
+      movie_obj.id = movie['id']
+      source_obj = @sources.find { |source| source.name == movie['source'] }
+      movie_obj.add_source(source_obj)
+      @movies << movie_obj
+    end
+  end
 end
