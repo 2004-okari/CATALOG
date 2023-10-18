afee62bef8b438630485a0cfea58d0b5417995a1
okari <nyandika15@gmail.com>
2023-10-18 11:42:02 +0300

    Add schema file

---
+++ spec/music_album_spec.rb
@@ -0,0 +1,17 @@
+require_relative '../classes/music_album'
+require_relative '../classes/item'
+
+describe 'MusicAlbum' do
+  context '#can_be_archived' do
+    it 'is archived when on Spotify' do
+      album = MusicAlbum.new(true)
+      item = Item.new(Date.new(2010, 1, 1))
+      expect(album.send(:can_be_archived?)).to be true
+    end
+
+    it 'is not archived when not on Spotify' do
+      album = MusicAlbum.new(false)
+      expect(album.send(:can_be_archived?)).to be false
+    end
+  end
+end
