2b7b52cda606a0fa1d0ae148dbfd517c8987b1c8
okari <nyandika15@gmail.com>
2023-10-18 11:46:05 +0300

    Update item.rb

--- classes/item.rb
+++ classes/item.rb
@@ -8,9 +8,6 @@ class Item
 
   def initialize(publish_date)
     @id = Random.rand(1..1000)
-    @genre = genre
-    @source = source
-    @label = label
     @publish_date = publish_date
     @archived = false
   end
@@ -18,6 +15,21 @@ class Item
   def add_author(author)
     @author = author
     author.items << self unless author.items.include?(self)
+  end
+
+  def add_label(label)
+    @label = label
+    label.items << self unless label.items.include?(self)
+  end
+
+  def add_genre(genre)
+    @genre = genre
+    genre.items << self unless genre.items.include?(self)
+  end
+
+  def add_source(source)
+    @source = source
+    source.items << self unless source.items.include?(self)
   end
 
   def move_to_archive
