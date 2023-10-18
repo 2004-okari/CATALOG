e2c2812ba3f3c0d12cdeed3625b2c47c9c1c47f6
okari <nyandika15@gmail.com>
2023-10-18 14:15:45 +0300

    Added console manager

--- main.rb
+++ main.rb
@@ -1,25 +1,8 @@
-# frozen_string_literal: true
-# # frozen_string_literal: true
+require_relative 'user_interactions'
 
-# class Main
-#   def greeting
-#     puts 'Hello, welcome to the catalog app'
-#   end
+def main
+  app = UserInteractions.new
+  app.run
+end
 
-#   def user_options
-#     puts 'Please select a number: '
-#     puts '1. List all books'
-#     puts '2. List all music albums'
-#     puts '3. List all movies'
-#     puts '4. List of games'
-#     puts "5. List all genres (e.g 'Comedy', 'Thriller')"
-#     puts "6. List all labels (e.g. 'Gift', 'New')"
-#     puts "7. List all authors (e.g. 'Stephen King')"
-#     puts "8. List all sources (e.g. 'From a friend', 'Online shop')"
-#     puts '9. Add a book'
-#     puts '10. Add a music album'
-#     puts '11. Add a movie'
-#     puts '12. Add a game'
-#     puts '13. Exit'
-#   end
-# end
+main
---
+++ user_interactions.rb
@@ -0,0 +1,57 @@
+require_relative 'app'
+
+class UserInteractions
+  def initialize
+    @catalog = App.new
+  end
+
+  def run
+    puts 'Welcome to the Catalog App!'
+    loop do
+      show_options
+      option = gets.chomp.to_i
+      break if option == 13
+
+      call_option(option)
+    end
+  end
+
+  def show_options
+    puts <<~OPTIONS
+      Please choose an option by entering a number:
+      1 - List all books
+      2 - List all music albums
+      3 - List all movies
+      4 - List all games
+      5 - List all genres
+      6 - List all labels
+      7 - List all authors
+      8 - List all sources
+      9 - Add a book
+      10 - Add a music album
+      11 - Add a movie
+      12 - Add a game
+      13 - Exit
+    OPTIONS
+  end
+
+  def call_option(option)
+    option_lookup = {
+      1 => :list_all_books,
+      2 => :list_music_album,
+      3 => :display_all_movies,
+      4 => :list_games,
+      5 => :list_genre,
+      6 => :list_all_labels,
+      7 => :list_authors,
+      8 => :display_all_sources,
+      9 => :add_book,
+      10 => :add_music_album,
+      11 => :add_movie,
+      12 => :add_game,
+      13 => :exit
+    }
+
+    @catalog.public_send(option_lookup[option])
+  end
+end
