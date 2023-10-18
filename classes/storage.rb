d699fde7b906be06e53686b91125df1fa6cd5296
Newton Ombese <newtonombese1@gmail.com>
2023-10-18 19:17:40 +0300

    Working for storing of sources and movies

--- classes/storage.rb
+++ classes/storage.rb
@@ -21,4 +21,26 @@ class Storage
     end
     File.write('data/labels.json', JSON.pretty_generate(json_labels))
   end
+
+  def save_movies
+    movie_hash = @movies.map do |movie|
+      {
+        id: movie.id,
+        publish_date: movie.publish_date,
+        silent: movie.silent,
+        source: movie.source.name
+      }
+    end
+    File.write('data/movie.json', JSON.pretty_generate(movie_hash))
+  end
+
+  def save_source
+    source_hash = @sources.map do |source|
+      {
+        id: source.id,
+        name: source.name
+      }
+    end
+    File.write('data/source.json', JSON.pretty_generate(source_hash))
+  end
 end
