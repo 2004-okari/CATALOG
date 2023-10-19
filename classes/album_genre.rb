057f4c9005d26960bae9d53ecbe0bdb8cca523c9
Wineshuga <uzochukwuwinnie@gmail.com>
2023-10-19 15:05:55 +0100

    Reduce length of methods

--- classes/album_genre.rb
+++ classes/album_genre.rb
@@ -17,32 +17,20 @@ module AlbumGenre
       puts 'No music albums found. Add a music album'
     else
       @music_album.each_with_index do |album, index|
-        if album.on_spotify
-          puts "#{index + 1}) Music Album: On Spotify, Genre => \"#{album.genre}\""
-        else
-          puts "#{index + 1}) Music Album: Not on Spotify, Genre => \"#{album.genre}\""
-        end
+        puts "#{index + 1}) Music Album: On Spotify? => #{album.on_spotify}, Genre => \"#{album.genre}\""
       end
     end
   end
 
   def add_music_album
     print 'Is it on Spotify(Y/N): '
-    spotify = gets.chomp
-    if spotify.downcase == 'y'
-      spotify = true
-    elsif spotify.downcase == 'n'
-      spotify = false
-    else
-      print 'Invalid answer'
-      exit
-    end
+    spotify = gets.chomp.downcase == 'y'
     puts "What's the genre?: "
     genre_name = gets.chomp
     new_genre = Genre.new(genre_name)
     new_music_album = MusicAlbum.new(spotify, genre_name)
-    @genre << new_genre unless @genre.include? new_genre
+    @genre << new_genre unless @genre.include?(new_genre)
     @music_album << new_music_album
-    print 'Music album created!'
+    puts 'Music album created!'
   end
 end
