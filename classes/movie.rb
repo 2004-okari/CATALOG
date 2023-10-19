7425fb1f73f6629fe1a0190fac0ec744cacb76ba
Newton Ombese <newtonombese1@gmail.com>
2023-10-19 19:21:42 +0300

    Work on the movie methods

---
+++ classes/movie.rb
@@ -0,0 +1,16 @@
+require_relative 'source'
+require_relative 'item'
+
+# Movie represents an list of movies
+class Movie < Item
+  attr_accessor :archived, :silent
+
+  def initialize(publish_date, silent:)
+    super(publish_date)
+    @silent = silent
+  end
+
+  def can_be_archived?
+    super && @silent
+  end
+end
